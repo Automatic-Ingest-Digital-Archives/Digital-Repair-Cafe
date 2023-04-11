4c677782f02bb6b855593ea83618b284dd77aad0
nvanderperren <ik.ben.nas@gmail.com>
2023-04-11 12:07:31 +0200

    add export script

---
+++ scripts/export_FAT_images.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+create_folder () {
+    echo "creating folder..."
+    file=$1
+    filename=$(basename $file)
+    echo $filename
+    mkdir "${file%.*}"
+}
+
+read_fat_images () {
+    images="$1"
+    for file in "$images/*.sh"
+    do
+        create_folder $file
+        filename=${file%.*}
+        mdir -/ -a -i $file > "$filename"_index.txt
+        mcopy -psmi $file ::* "$filename"
+    done
+}
+
+read_hfs_images () {
+    images="$1"
+    for file in "$images/*.sh"
+    do
+        create_folder $file
+        hmount $file
+        filename=${file%.*}
+        hls -ialR > "$filename"_index.txt
+        hcopy -m :* "$filename/"
+    done
+}
+
+while getopts 'f:h:' OPTION; do
+  case $OPTION in
+    f) 
+      arg="$OPTARG"
+      read_fat_images $arg
+      ;;
+    h)
+      arg="$OPTARG"
+      read_hfs_images $arg
+      ;;
+    ?) 
+      echo "Usage: $(basename $0) [-f] [-h]"
+      exit 1
+      ;;
+    esac
+done
